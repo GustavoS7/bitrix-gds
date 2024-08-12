d3f4a91cd901b7f7b81f38b2bc3c2ef1718c8b08
Gustavo dos Santos <gustavods2003@outlook.com>
2024-08-12 10:16:33 -0300

    [fix] call batch params

--- src/presentation/app.ts
+++ src/presentation/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-void */
 import { ContactUseCase, DealUseCase } from '../application/use-cases';
 import { TBatchResponse } from '../core/shared/response';
 import { IBatchCallerResponse } from './interfaces';
@@ -53,24 +54,62 @@ export class App {
         params?: Record<string, any>;
       };
     }): Promise<TBatchResponse<any>> => {
-      let cmd: any = [];
-      const keys = Object.keys(methods);
+      const cmd = Object.entries(methods).reduce(
+        (calls, [name, { method, params }]) => {
+          const stringifiedParams = params ? `?${qs.stringify(params)}` : '';
 
-      keys.forEach((el) => {
-        cmd.push(
-          `cmd[${el}]=${methods[el].method}?${qs.stringify(methods[el].params)}`,
-        );
-      });
-
-      cmd = cmd.join('&') + '';
+          return {
+            ...calls,
+            [`cmd[${name}]`]: `${method}${stringifiedParams}`,
+          };
+        },
+        {},
+      );
 
       try {
-        const { data } = await this.api.post(`${Methods.BATCH}?${cmd}`);
-
+        const { data } = await this.api.post(`${Methods.BATCH}`, { cmd });
         return data;
       } catch (error) {
         throw new Error();
       }
     };
   }
+
+  // Native toQuery code
+  // private isObj(a: any) {
+  //   if (!!a && a.constructor === Object) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
+  // private _st(z: any, g: any) {
+  //   return '' + (g !== '' ? '[' : '') + z + (g !== '' ? ']' : '');
+  // }
+
+  // private toQuery(params: any, skipobjects?: any, prefix?: any) {
+  //   if (skipobjects === void 0) {
+  //     skipobjects = false;
+  //   }
+  //   if (prefix === void 0) {
+  //     prefix = '';
+  //   }
+  //   let result = '';
+  //   if (typeof params !== 'object') {
+  //     return prefix + '=' + encodeURIComponent(params) + '&';
+  //   }
+  //   for (const param in params) {
+  //     const c = '' + prefix + this._st(param, prefix);
+  //     if (this.isObj(params[param]) && !skipobjects) {
+  //       result += this.toQuery(params[param], false, '' + c);
+  //     } else if (Array.isArray(params[param]) && !skipobjects) {
+  //       params[param].forEach((item, ind) => {
+  //         result += this.toQuery(item, false, c + '[' + ind + ']');
+  //       });
+  //     } else {
+  //       result += c + '=' + encodeURIComponent(params[param]) + '&';
+  //     }
+  //   }
+  //   return result;
+  // }
 }
